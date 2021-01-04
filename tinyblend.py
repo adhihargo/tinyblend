fafeef5b45ba274350172841be6912c591fa6bcf
Adhi Hargo <cadmus.sw@gmail.com>
2021-01-04 08:08:09 +0700

    Cleanup (license info as comment)

--- tinyblend.py
+++ tinyblend.py
@@ -1,4 +1,32 @@
 # -*- coding: utf-8 -*-
+# MIT License
+#
+# Copyright (c) 2016 Gabriel Dubé
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import re
+from collections import namedtuple
+from enum import Enum
+from struct import Struct
+from weakref import ref
+
 """
 Assets loader for the blender file format (.blend).
 
@@ -20,36 +48,6 @@ print(blend_file.tree('World'))
 
 Author: Gabriel Dube
 """
-
-"""
-MIT License
-
-Copyright (c) 2016 Gabriel Dubé
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-"""
-
-from struct import Struct
-from enum import Enum
-from collections import namedtuple
-from weakref import ref
-import re
 
 # List of base types found in blend fields and their struct char representation.
 _BASE_TYPES = {'float':'f', 'double':'d', 'int':'i', 'short':'h', 'ushort': "H", 'char':'c', 'char': 'B', 'long': 'l', 'ulong': 'L', 'uint64_t':'Q', 'int64_t':'q'}
